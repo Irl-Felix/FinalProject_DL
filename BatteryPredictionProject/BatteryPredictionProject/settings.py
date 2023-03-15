4d1bf2fdfec21f9bd3f7cd28b574ccedca5d3e2d
Javio Felix <javifsk@gmail.com>
2023-03-15 23:12:40 +0800

    register template folder

--- BatteryPredictionProject/BatteryPredictionProject/settings.py
+++ BatteryPredictionProject/BatteryPredictionProject/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'BatteryPredictionProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'mainapp' /'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
